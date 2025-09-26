d4204a85279bb64c701f5635c04c3d3cf05298c3
Sabrina Cezario <sabrina.cezario@escola.pr.gov.br>
2025-09-26 11:46:59 -0300

    Create SENHA.cpp

---
+++ SENHA.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int senha = 0;
+	printf("Digite uma senha:\n"); 
+	scanf("%d", &senha);
+	
+	for (int i = 0; i<10; i++) {
+		for(int x = 0; x<10; x++){
+			printf("valor: %d %d \n", i,x);
+           
+		
+		}
+	}
+}
